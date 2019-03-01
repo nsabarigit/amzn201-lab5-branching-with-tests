716a367c81b430735d51cc3b24300865236e1d1c
Natarajan <nsabari@amazon.com>
2019-02-28 19:59:31 -0800

    Implement sum function on branch sum

--- app.js
+++ app.js
@@ -8,12 +8,18 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
-
+function sum(a,b) { //eslint-disable-line
+  var result = Number(a) + Number(b);
+  console.log("Sum :: Result :", result);
+  var sumResultArray = [];
+  sumResultArray[0] = result;
+  sumResultArray[1] = "The sum of " + a + " and " + b + " is " + result + ".";
+  console.log("sumResultArray[1]::", sumResultArray[1]);
+  return sumResultArray;
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
